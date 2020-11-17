148c1e7de664f4caad81acfd324873236868b801
khristinasukhomlyn <khristina.sukhomlin@gmail.com>
2020-11-17 23:49:04 +0200

    добавляет решение 5
    доработать 5, 6, 7
    решить 8

--- js/task-05.js
+++ js/task-05.js
@@ -2,3 +2,15 @@
 //     - input(событие input), подставляет его текущее значение в
 // span#name - output.
 // Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+console.log("output", output);
+
+input.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  input.value === " "
+    ? (output.textContent = "незнакомец")
+    : (output.textContent = event.currentTarget.value);
+}
