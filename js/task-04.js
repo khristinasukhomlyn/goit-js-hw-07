0841e12ce5178121503e99462128f8560a6bb2bf
khristinasukhomlyn <khristina.sukhomlin@gmail.com>
2020-11-17 23:29:37 +0200

    Добавляет решение задачи 4

--- js/task-04.js
+++ js/task-04.js
@@ -7,3 +7,14 @@
 // счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление
 // интерфейса
+
+const incremButton = document.querySelector("button[data-action='increment']");
+const decremButton = document.querySelector("button[data-action='decrement']");
+let counterValue = document.querySelector("#value");
+
+decremButton.addEventListener("click", () => {
+  if (counterValue.textContent > 0) counterValue.textContent -= 1;
+});
+incremButton.addEventListener("click", () => {
+  counterValue.textContent = +counterValue.textContent + 1;
+});
